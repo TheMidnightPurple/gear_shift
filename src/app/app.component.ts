a274ab45a4134ced788f64d7feb2fa238df7965b
Carlos Preto <cjpreto31@gmail.com>
2024-03-08 17:50:03 +0000

    new car

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,23 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { MainComponent } from './layout/main/main.component';
+import { IMAGE_CONFIG } from '@angular/common';
 
 @Component({
   standalone: true,
   selector: 'app-root',
   imports: [RouterOutlet, MainComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  providers: [
+    {
+      provide: IMAGE_CONFIG,
+      useValue: {
+        disableImageSizeWarning: true,
+        disableImageLazyLoadWarning: true,
+      },
+    },
+  ],
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'cardCollector';
